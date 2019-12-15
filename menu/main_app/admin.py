fcf1457ca56d3521bc23029ffabf6f3f97276f06
Yegor Bitensky <yegorbitensky@gmail.com>
2019-12-16 00:23:39 +0500

    add models to admin

--- menu/main_app/admin.py
+++ menu/main_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from main_app.models import Allergen, Category, Dish
+
+
+@admin.register(Allergen)
+class AllergenAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    pass
--- menu/main_app/models.py
+++ menu/main_app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Allergen(models.Model):
+    class Meta:
+        verbose_name = 'Аллерген'
+        verbose_name_plural = 'Аллергены'
+
     name = models.CharField('Название', max_length=255)
 
     def __str__(self):
@@ -9,6 +13,10 @@ class Allergen(models.Model):
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
     name = models.CharField('Название', max_length=255)
 
     def __str__(self):
@@ -16,6 +24,10 @@ class Category(models.Model):
 
 
 class Dish(models.Model):
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
     name = models.CharField('Название', max_length=255)
     squirrels = models.DecimalField('Белки', max_digits=9, decimal_places=2)
     fats = models.DecimalField('Жиры', max_digits=9, decimal_places=2)
@@ -23,8 +35,8 @@ class Dish(models.Model):
     energy = models.PositiveIntegerField('Энергетическая ценность')
     price = models.DecimalField('Цена', max_digits=9, decimal_places=2)
     picture = models.ImageField('Фото', upload_to='dishes/%Y/%m/%d', max_length=255)
-    allergens = models.ManyToManyField(Allergen)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    allergens = models.ManyToManyField(Allergen, verbose_name='Аллергены')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
