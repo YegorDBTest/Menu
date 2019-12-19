6f62214a57f066c518eb049a9ad93c50b8a2d30a
Yegor Bitensky <yegorbitensky@gmail.com>
2019-12-20 02:57:43 +0500

    draw order items

--- menu/main_app/serializers.py
+++ menu/main_app/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
 
-from main_app.models import Dish
+from main_app.models import Allergen, Dish
 
 
 class DishSerializer(serializers.ModelSerializer):
-    allergens = serializers.StringRelatedField(many=True, required=False)
+    allergens = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        queryset=Allergen.objects.all(),
+        slug_field='name'
+    )
 
     class Meta:
         model = Dish
-        fields = "__all__"
+        fields = (
+            'name',
+            'squirrels',
+            'fats',
+            'carbohydrates',
+            'energy',
+            'price',
+            'picture',
+            'allergens',
+            'category'
+        )
--- menu/main_app/utils.py
+++ menu/main_app/utils.py
@@ -1,22 +1,24 @@
 from decimal import Decimal
 
 
+def get_dish_data(dish):
+    return {
+        'category': dish.category.id,
+        'id': dish.id,
+        'name': dish.name,
+        'picture': dish.picture.url,
+        'squirrels': dish.squirrels,
+        'fats': dish.fats,
+        'carbohydrates': dish.carbohydrates,
+        'energy': dish.energy,
+        'price': dish.price,
+        'allergens': dish.allergens.values_list('name', flat=True)
+    }
+
+
 def aggregate_dishes_data(dishes):
     return {
-        'dishes': [{
-                'category': dish.category.id,
-                'id': dish.id,
-                'name': dish.name,
-                'picture': dish.picture.url,
-                'squirrels': dish.squirrels,
-                'fats': dish.fats,
-                'carbohydrates': dish.carbohydrates,
-                'energy': dish.energy,
-                'price': dish.price,
-                'allergens': dish.allergens.values_list('name', flat=True)
-            }
-            for dish in dishes
-        ],
+        'dishes': [get_dish_data(dish) for dish in dishes],
     }
 
 
@@ -24,32 +26,15 @@ def aggregate_dishes_data_with_total(dishes):
     data = {
         'dishes': [],
         'total': {
-            'squirrels': Decimal(0),
-            'fats': Decimal(0),
-            'carbohydrates': Decimal(0),
-            'energy': 0,
             'price': Decimal(0),
             'allergens': set(),
         },
     }
 
     for dish in dishes:
-        dish_data = {
-            'category': dish.category.id,
-            'id': dish.id,
-            'name': dish.name,
-            'picture': dish.picture.url
-        }
-
-        for key in ('squirrels', 'fats', 'carbohydrates', 'energy', 'price'):
-            value = getattr(dish, key)
-            dish_data[key] = value
-            data['total'][key] += value
-
-        allergens = set(dish.allergens.values_list('name', flat=True))
-        dish_data['allergens'] = allergens
-        data['total']['allergens'] |= allergens
-
+        dish_data = get_dish_data(dish)
+        data['total']['price'] += dish_data['price']
+        data['total']['allergens'] |= set(dish_data['allergens'])
         data['dishes'].append(dish_data)
 
     return data
